0af1c3c18c5718020f61c4bed4edbbc86c3a745a
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-31 00:20:34 +0530

    working on header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./Header";
+
 import "./App.css";
 
 function App() {
@@ -8,14 +10,15 @@ function App() {
             <div className="app">
                 <Switch>
                     <Route path="/checkout">
-						<h1>Checkout Page</h1>
-					</Route>
+                        <h1>Checkout Page</h1>
+                    </Route>
                     <Route path="/login">
-						<h1>Login Page</h1>
-					</Route>
+                        <h1>Login Page</h1>
+                    </Route>
                     <Route extact path="/">
-						<h1>Home Page</h1>
-					</Route>
+                        <Header />
+                        <h1>Home Page</h1>
+                    </Route>
                 </Switch>
             </div>
         </Router>
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+    return (
+        <nav className="header">
+            {/* Logo on left */}
+            <img
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="Amazon Logo"
+                className="header__logo"
+            />
+            {/* Search Box */}
+            {/* 3 Links */}
+            {/* Basket */}
+        </nav>
+    );
+}
+
+export default Header;
