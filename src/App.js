b32a3a6262a62d8e3a9bc80d442089ce7eedd88a
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-31 00:02:48 +0530

    setup basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route path="/checkout">
+						<h1>Checkout Page</h1>
+					</Route>
+                    <Route path="/login">
+						<h1>Login Page</h1>
+					</Route>
+                    <Route extact path="/">
+						<h1>Home Page</h1>
+					</Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
