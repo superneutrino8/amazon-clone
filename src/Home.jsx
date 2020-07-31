eff8ec9f878a17c9c2dfc2ceec706fbd2c760e66
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-31 23:50:22 +0530

    added product component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Product from "./Product";
 
 import "./Home.css";
 
@@ -10,6 +11,55 @@ function Home() {
                 src="https://images-na.ssl-images-amazon.com/images/G/15/kindle/journeys/M2ZmNTBiMTEt/M2ZmNTBiMTEt-YjVmMjkzY2It-w1500._CB429544641_.jpg"
                 alt=""
             />
+            {/* Product id, title, price, rating, image */}
+            <div className="home__row">
+                <Product
+                    id={5421}
+                    title="Sapiens: A Brief History of Humankind"
+                    price={399}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/41yu2qXhXXL._SX324_BO1,204,203,200_.jpg"
+                />
+                <Product
+                    id={4853}
+                    title="Elon Musk: How the Billionaire CEO of SpaceX and Tesla is Shaping our Future"
+                    price={615}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/51I-QgidWHL._SX322_BO1,204,203,200_.jpg"
+                />
+            </div>
+            <div className="home__row">
+                <Product
+                    id={3652}
+                    title="The Last of Us 2 -Standard Edition (PS4)"
+                    price={3999}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/81l%2BhkZEABL._SL1500_.jpg"
+                />
+                <Product
+                    id={4156}
+                    title="Microsoft Surface Pro X "
+                    price={96,650}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/51ZqIfidWfL._SL1200_.jpg"
+                />
+                <Product
+                    id={7564}
+                    title="Apple MacBook Pro"
+                    price={1,99,999}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/71L2iBSyyOL._SL1500_.jpg"
+                />
+            </div>
+            <div className="home__row">
+                <Product
+                    id={3256}
+                    title="Samsung LC49HG90DMUXEN 48.9-inch Ultra Wide Curved Monitor (Black)"
+                    price={1,40,800}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/81vlA84pg6L._SL1500_.jpg"
+                />
+            </div>
         </div>
     );
 }
---
+++ src/Product.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Product.css";
+
+function Product({ id, title, price, rating, image }) {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>‎₹‎</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p key={i}>
+                                <span role="img" aria-label="star">
+                                    ⭐
+                                </span>
+                            </p>
+                        ))}
+                </div>
+            </div>
+            <img src={image} alt={title} />
+            <button>Add to Basket</button>
+        </div>
+    );
+}
+
+export default Product;
