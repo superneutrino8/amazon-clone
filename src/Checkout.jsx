9f7186899b5bb78d59b78e3ed95a478ecec75e9b
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-08-19 23:27:10 +0530

    styled checkout page

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -15,7 +15,7 @@ function Checkout() {
                 alt=""
             />
             {basket?.length === 0 ? (
-                <div className="">
+                <div className="checkout__title">
                     <h2>Your Shopping Basket is empty</h2>
                     <p>
                         You have no items in your basket. To buy one or more
@@ -23,7 +23,7 @@ function Checkout() {
                     </p>
                 </div>
             ) : (
-                <div className="">
+                <div className="checkout__title">
                     <h2>Your Shopping Basket</h2>
                     {basket?.map((item) => (
                         <CheckoutProduct
